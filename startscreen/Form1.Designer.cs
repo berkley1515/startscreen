f75497726273de4fb7167f3a5fb8367c8db9a0df
Berkley Fair <berkley1515@gmail.com>
2017-09-18 13:03:49 -0400

    Add project files.

---
+++ startscreen/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace startscreen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startbutton = new System.Windows.Forms.Button();
+            this.counter = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // startbutton
+            // 
+            this.startbutton.BackColor = System.Drawing.Color.Aqua;
+            this.startbutton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Aqua;
+            this.startbutton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Blue;
+            this.startbutton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.startbutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startbutton.Location = new System.Drawing.Point(262, 159);
+            this.startbutton.Name = "startbutton";
+            this.startbutton.Size = new System.Drawing.Size(145, 59);
+            this.startbutton.TabIndex = 0;
+            this.startbutton.Text = "Start";
+            this.startbutton.UseVisualStyleBackColor = false;
+            this.startbutton.Click += new System.EventHandler(this.startbutton_Click);
+            // 
+            // counter
+            // 
+            this.counter.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.counter.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.counter.Location = new System.Drawing.Point(283, 251);
+            this.counter.Name = "counter";
+            this.counter.Size = new System.Drawing.Size(100, 58);
+            this.counter.TabIndex = 1;
+            this.counter.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.MidnightBlue;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(704, 441);
+            this.Controls.Add(this.counter);
+            this.Controls.Add(this.startbutton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startbutton;
+        private System.Windows.Forms.Label counter;
+    }
+}
+
---
+++ startscreen/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+
+
+
+
+namespace startscreen
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void startbutton_Click(object sender, EventArgs e)
+        {
+            //sounds
+            SoundPlayer pickleRick = new SoundPlayer(Properties.Resources.Im_Pickle_Rick_);
+            //countdown sound
+            SoundPlayer beep = new SoundPlayer(Properties.Resources.beep_02);
+
+            //graphics settings
+            Graphics formGraphics = this.CreateGraphics();
+            Pen drawPen = new Pen(Color.Red, 10);
+            SolidBrush drawBrush = new SolidBrush(Color.Yellow);
+            Font drawFont = new Font("Impact", 41, FontStyle.Bold);
+            SolidBrush drawBrushFont = new SolidBrush(Color.Red);
+            Font drawFont2 = new Font("Arial", 18, FontStyle.Bold);
+            SolidBrush drawBrush2 = new SolidBrush(Color.Blue);
+            Font drawFontsmall = new Font("Arial", 7, FontStyle.Regular);
+            SolidBrush drawBrushsmall = new SolidBrush(Color.LimeGreen);
+
+
+            startbutton.Visible = false;
+            beep.Play();
+            counter.Text = "3";
+            Refresh();
+            Thread.Sleep(1000);
+
+            counter.Text = "2";
+            beep.Play();
+            Refresh();
+            Thread.Sleep(1000);
+
+            counter.Text = "1";
+            beep.Play();
+            Refresh();
+            Thread.Sleep(1000);
+
+            counter.Text = "Go!";
+            pickleRick.Play();
+            Refresh();
+            Thread.Sleep(2000);
+
+            counter.Visible = false;
+            this.BackgroundImage = (Properties.Resources.CMzXgYUver2);
+            Refresh();
+
+            formGraphics.FillRectangle(drawBrush, 150, 90, 400, 100);
+            formGraphics.DrawString("Rick and Morty", drawFont, drawBrushFont, 170, 90);
+            formGraphics.DrawString("Logo", drawFont2, drawBrush2, 310, 150);
+
+            formGraphics.TranslateTransform(385, 160);
+            formGraphics.RotateTransform(90);
+            formGraphics.DrawString("TM", drawFontsmall, drawBrushsmall, new Rectangle());
+            formGraphics.ResetTransform();
+            
+        }
+    }
+}
